11f6a02528d711e68b817cb1d9dd0cf62fbd9555
Shawn Pan <shawn137080@gmail.com>
2023-05-07 21:13:11 -0400

    first

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env.HOST,
+  port: env.PORT,
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
